d7b803b0a3ff11362c40f0d7841b42af32eb5eb7
j801h <75646515+j801h@users.noreply.github.com>
2023-04-03 02:40:27 -0700

    skill circle rendering fix

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -6,18 +6,4 @@ $(document).ready(function() {
       template: '<div class="tooltip custom-tooltip" role="tooltip"><div class="arrow"></div><div class="tooltip-inner">' + tooltipContent + '</div></div>'
     });
   });
-
-  // Initialize circular progress bars using Circles
-  {% for skill in site.data.settings.skills %}
-  Circles.create({
-    id: 'circle-{{ forloop.index }}',
-    radius: 60,
-    value: {{ skill.value }},
-    maxValue: 100,
-    width: 10,
-    text: function(value) { return value + '%'; },
-    colors: ['#eee', '{{ skill.color }}'],
-    duration: 400,
-  });
-  {% endfor %}
 });
